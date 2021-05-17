169f707b7f537e41cc8eb3a1d9c0a234449865bb
Isaiah Jenkins <icjenkins@dons.usfca.edu>
2021-05-16 21:48:41 -0700

    Added logo and background color

---
+++ src/features/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './upicon.png';
+import Tilt from 'react-parallax-tilt';
+
+const Logo = () => {
+	return (
+		<div className='flex pa4'>
+			<Tilt tiltMaxAngleX='50' tiltMaxAngleY='50'>
+				<img className='br-100 ba bw2 b--white h3 w3' src={logo} alt='Logo' />
+			</Tilt>
+		</div>
+	);
+};
+
+export default Logo;
