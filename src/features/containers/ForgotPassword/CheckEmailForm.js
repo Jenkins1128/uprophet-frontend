fb3e031d8846105f489b352726d50bb10c882dad
Isaiah Jenkins <icjenkins@dons.usfca.edu>
2021-07-19 10:27:12 -0700

    Changed from div to article CheckEmailForm

--- src/features/containers/ForgotPassword/CheckEmailForm.js
+++ src/features/containers/ForgotPassword/CheckEmailForm.js
@@ -1,16 +1,14 @@
 const CheckEmailForm = ({ Link }) => {
 	return (
-		<div className=' br2 ba pv4 black-80 dark-gray b--black-10 br4 w-60 mw6 shadow-5 measure center'>
-			<div className='flex flex-column mh4'>
-				<h1>Check email for temporary password.</h1>
-				<Link to='/changepassword' className='center b w-75 pv2 input-reset ba br4 b--black bg-light-green black grow pointer hover-white no-underline f6 ' type='submit'>
-					Change Password
-				</Link>
-				<Link to='/signin' className='center b w-75 mt3 pv2 input-reset ba br4 b--black bg-light-green black grow pointer hover-white no-underline f6 ' type='submit'>
-					Sign In
-				</Link>
-			</div>
-		</div>
+		<article className='flex flex-column mh4 br2 ba pv4 black-80 dark-gray b--black-10 br4 w-60 mw6 shadow-5 measure center'>
+			<h1>Check email for temporary password.</h1>
+			<Link to='/changepassword' className='center b w-75 pv2 input-reset ba br4 b--black bg-light-green black grow pointer hover-white no-underline f6 ' type='submit'>
+				Change Password
+			</Link>
+			<Link to='/signin' className='center b w-75 mt3 pv2 input-reset ba br4 b--black bg-light-green black grow pointer hover-white no-underline f6 ' type='submit'>
+				Sign In
+			</Link>
+		</article>
 	);
 };
 
